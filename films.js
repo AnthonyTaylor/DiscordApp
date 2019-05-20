325d17a72e28a83831b7028b05bc0819294adb69
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2019-05-20 11:10:01 +0100

    Docker init

--- films.js
+++ films.js
@@ -3,6 +3,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const MongoUrl = process.env.DB_CONN;
 const DB = process.env.DB;
+const coll = process.env.coll;
 
 //message passed from chat.js
 this.chat = function(msg, client){
@@ -13,36 +14,40 @@ this.chat = function(msg, client){
 	
 	switch(command[0].toLowerCase()) {
 		case "list":
-			findAll();
-			break;
+		findAll();
+		break;
 		case "seen":
-			updateDB('seen', command[1].toLowerCase(), command[2].toLowerCase());	
-			break;
+		updateDB('seen', parseInt(command[1]), command[2].toLowerCase());	
+		break;
 		case "unseen":
-			updateDB('unseen', command[1].toLowerCase(), command[2].toLowerCase());	
-			break;
+		updateDB('unseen', parseInt(command[1]), command[2].toLowerCase());	
+		break;
 		default:
-			//ensures both are strings prior to use
-			var comm1 = command[0].toLowerCase().toString();
-			var comm2 = command[1].toLowerCase().toString();
-			
-			//prepares Mongo query
-			//note: unless otherwise stated the variable at the start of the object will be treated as plaintext
-			//in order to use the value held within the variable it needs to be enclosed in square brackets
-			var mQuery = { [comm1]: comm2 };
-			
-			findSpecific(mQuery);
-			break;
+		//ensures both are strings prior to use
+		var comm1 = command[0].toLowerCase();
+		var comm2 = command[1].toLowerCase();
+		
+		//prepares Mongo query
+		//note: unless otherwise stated the variable at the start of the object will be treated as plaintext
+		//in order to use the value held within the variable it needs to be enclosed in square brackets
+		var mQuery = { [comm1]: comm2 };
+		console.log(mQuery)
+		findSpecific(mQuery);
+		break;
 	}
 	
+	/*
+	params
+	mQuery: (object) - search query, such as ant: "seen"
+	*/
 	function findSpecific(mQuery){
 		//connect to the database and collection
-		MongoClient.connect(MongoUrl, function(err, db) {
+		MongoClient.connect(MongoUrl, {useNewUrlParser: true }, function(err, db) {
 			if (err) throw err;
 			var dbo = db.db(DB);
 			
 			//send the query to the database and convert the response to an array
-			dbo.collection("main").find(mQuery, { _id: 0, ID: 1, name: 1}).toArray(function(err, result) {
+			dbo.collection(coll).find(mQuery).toArray(function(err, result) {
 				if (err) throw err;
 				//console.log(JSON.stringify(result, null, 2));
 				
@@ -64,12 +69,12 @@ this.chat = function(msg, client){
 	
 	function findAll(){
 		//connect to the database and collection
-		MongoClient.connect(MongoUrl, function(err, db) {
+		MongoClient.connect(MongoUrl, {useNewUrlParser: true }, function(err, db) {
 			if (err) throw err;
 			var dbo = db.db(DB);
-			
+			//console.log("Number of items:" + dbo.collection(coll).count());
 			//send the query to the database and convert the response to an array
-			dbo.collection("main").find().toArray(function(err, result) {
+			dbo.collection(coll).find().toArray(function(err, result) {
 				
 				//for each object in the array, add the ID and name to the response
 				result.forEach(function(obj) { response += obj.ID + " " + obj.name + "\n";});
@@ -83,35 +88,38 @@ this.chat = function(msg, client){
 	
 	/*
 	params
-		num: ID of the film
-		name: name of the person to be updarted
-		value: true / false
+	val: (string) - seen / unseen
+	num: (number) - ID of the film
+	name: (string) - name of the person to be updated
 	*/
 	function updateDB(val, num, name){
 		//connect to the database and collection
-
-		bWatched = (val = 'seen') ? true : false
-
-		MongoClient.connect(MongoUrl, function(err, db) {
+		MongoClient.connect(MongoUrl, {useNewUrlParser: true }, function(err, db) {
 			if (err) throw err;
-			var dbo = db.db(DB);
-			
-			dbo.collection("main").update(
-				{"ID" : num},
-				{$set: {[name]: bWatched}}
-				);
-				
-				msg.reply(`Film ${num} updated`);
-				db.close();
+			var dbo = db.db("Marvel");
+			var myquery = { ID: num };
+			var newvalues = { $set: {[name]: val} };
+
+			dbo.collection("Films").updateOne(myquery, newvalues, function(err, res) {
+			  if (err) throw err;
+			  if(res.result.nModified == 1){
+				console.log(`Film ${num} updated`);
+				var mQuery = { [name]: val };
+				findSpecific(mQuery)
+			  }else{
+				console.log('Failed to update');
+			  }  
+			  db.close();
 			});
-		}
+		  });
 	}
+}
 
-	/*
-	 *   /film seen [id] [name] – changing the syntax so that you can mark someone as seen or unseen.  Can these two commands reuse code?
-	 *   /film unseen [id] [name] – changing the syntax so that you can mark someone as seen or unseen.  Can these two commands reuse code?
-	 *   /film update [id] [title] – update the film title
-	 *   /film add [optional id] [title] (just uses next id if not specified) – Add a new movie, with the ID (integer) parameter being optional
-	 *   /unseen [name … name] – pass in a list of users and get a list of movies they haven’t seen
+/*
+*  √ /film seen [id] [name] – changing the syntax so that you can mark someone as seen or unseen.  Can these two commands reuse code?
+*  √ /film unseen [id] [name] – changing the syntax so that you can mark someone as seen or unseen.  Can these two commands reuse code?
+*   /film update [id] [title] – update the film title
+*   /film add [optional id] [title] (just uses next id if not specified) – Add a new movie, with the ID (integer) parameter being optional
+*   /unseen [name … name] – pass in a list of users and get a list of movies they haven’t seen
 
-  */
+*/
---
+++ mongo.js
@@ -0,0 +1,257 @@
+//setup
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+function createDB(DBname){
+	var urlWithDB = url + DBName;
+	MongoClient.connect(urlWithDB, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+		console.log("Database created!");
+		db.close();
+	});
+}
+function createColl(DBName, collName){
+	MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(DBName);
+		dbo.createCollection(collName, function(err, res) {
+			if (err) throw err;
+			console.log("Collection created!");
+			db.close();
+		});
+	});
+}
+function addDocs(DBName, collName){
+	MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(DBName);
+		var myobj = [
+			{
+				ID: 1,
+				name: "Iron Man",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 2,
+				name: "The Incredible Hulk",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 3,
+				name: "Iron Man 2",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 4,
+				name: "Thor",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 5,
+				name: "Captain America: The First Avenger",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 6,
+				name: "Marvel's The Avengers",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 7,
+				name: "Iron Man 3",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 8,
+				name: "Thor: The Dark World",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 9,
+				name: "Captain America: The Winter Soldier",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 10,
+				name: "Guardians of the Galaxy",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 11,
+				name: "Avengers: Age of Ultron",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 12,
+				name: "Ant Man",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 13,
+				name: "Captain America: Civil War",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 14,
+				name: "Doctor Strange",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 15,
+				name: "Guardians of the Galaxy Vol. 2",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 16,
+				name: "Spiderman: Homecoming",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 17,
+				name: "Thor: Ragnarok",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 18,
+				name: "Black Panther",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 19,
+				name: "Avengers: Infinity War",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 20,
+				name: "Ant-Man and the Wasp",
+				owned: "true",
+				ant: "seen",
+				carl: "seen"
+			},
+			{
+				ID: 21,
+				name: "Captain Marvel",
+				owned: "false",
+				ant: "unseen",
+				carl: "unseen"
+			},
+			{
+				ID: 22,
+				name: "Avengers: Endgame",
+				owned: "false",
+				ant: "unseen",
+				carl: "unseen"
+			},
+			{
+				ID: 23,
+				name: "Spider-Man: Far From Home",
+				owned: "false",
+				ant: "unseen",
+				carl: "unseen"
+			}
+		];
+		dbo.collection(collName).insertMany(myobj, function(err, res) {
+			if (err) throw err;
+			console.log("Number of documents inserted: " + res.insertedCount);
+			db.close();
+		});
+	});
+}
+function findDocs(DBName, collName, findQuery){
+	MongoClient.connect(url , { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(DBName);
+		dbo.collection(collName).find(findQuery).toArray(function(err, result) {
+			if (err) throw err;
+			console.log(result);
+			db.close();
+		});
+	});
+}
+function updateDocs(DBName, collName, num, name, val){
+	MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db(DBName);
+		var myquery = { ID: num };
+		var newvalues = { $set: {[name]: val} };
+		
+		dbo.collection(collName).updateOne(myquery, newvalues, function(err, res) {
+			if (err) throw err;
+			if(res.result.nModified == 1){
+				console.log(`Film ${num} updated`);
+				var mQuery = { [name]: val };
+				findSpecific(mQuery)
+			}else{
+				console.log('Failed to update');
+			}
+			
+			db.close();
+		});
+	});
+}
+function findDBs(url){
+	var MongoClient = require('mongodb').MongoClient;
+	
+	// Connect using MongoClient
+	MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+		// Use the admin database for the operation
+		var adminDb = db.admin();
+		// List all the available databases
+		adminDb.listDatabases(function(err, result) {
+			console.log(result.databases);
+			db.close();
+		});
+	});
+}
+
+var DBName = "Marvel";
+var  collName = "Films";
+var findQuery = {ID: 1}
+var num = 13;
+var name = "benedict"
+var val = "seen"
+
+//createDB(DBName);
+//createColl(DBName, collName);
+//addDocs(DBName, collName);
+findDocs(DBName, collName, findQuery);
+//updateDocs(DBName, Films, num, name, val);
+//findDBs(url);
